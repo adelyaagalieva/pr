03c34a62f6353ae7113fa636b3469589b5dd5f02
adelyaagalieva <34675378+adelyaagalieva@users.noreply.github.com>
2017-12-19 19:24:51 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 s = new Class1();
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            int Greater = s.Greater(x, y);
+            Console.WriteLine("Greater number from {0} and {1} is {2}", x, y, Greater);
+            Console.ReadKey();
+        }
+    }
+}
